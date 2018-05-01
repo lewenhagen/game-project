37688a8f8a2f478db307d6b4db8815199bd0539e
Kenneth Lewenhagen <lewenhagen@live.com>
2018-05-01 23:17:41 +0200

    after 8

--- app.js
+++ app.js
@@ -174,13 +174,54 @@ Bullet.update = function() {
 
 var DEBUG = true;
 
+var USERS = {
+    //username:password
+    "bob": "bob123",
+    "kenneth": "kenneth123",
+    "malin": "malin123"
+}
+
+var isValidPassword = function(data) {
+    return USERS[data.username] === data.password;
+}
+
+var isUserNameTaken = function(data) {
+    return USERS[data.username];
+}
+
+var addUser = function(data) {
+    return USERS[data.username] = data.password;
+}
+
 var io = require("socket.io")(serv, {});
 io.sockets.on("connection", (socket) => {
     console.log("socket connection!");
-
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
-    Player.onConnect(socket);
+
+
+    socket.on("signIn", (data) => {
+        if (isValidPassword(data)) {
+            Player.onConnect(socket);
+            socket.emit("signInResponse", {success: true});
+            console.log("Login success!");
+        } else {
+            socket.emit("signInResponse", {success: false});
+            console.log("Login failed!");
+        }
+    });
+
+    socket.on("signUp", (data) => {
+        if (!isUserNameTaken(data)) {
+            addUser(data)
+            socket.emit("signUpResponse", {success: true});
+            console.log("User added!");
+        } else {
+            socket.emit("signUpResponse", {success: false});
+            console.log("Username taken!");
+        }
+    });
+
 
     socket.on("disconnect", () => {
         delete SOCKET_LIST[socket.id];
